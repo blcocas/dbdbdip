e2c4a08370267dcb638c82af7ab04597e20260b2
HONG-PC\Hong <blcocas@naver.com>
2017-12-01 11:41:36 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/keyboard', function(req, res){
 
   const menu = {
       "type": 'buttons',
-      "buttons": ["뭐먹을래"]
+      "buttons": ["안에서 먹을래", "밖에서 먹을래"]
   };
 
   res.send(menu);
@@ -21,31 +21,83 @@ router.get('/keyboard', function(req, res){
 
 //message
 router.post('/message', (req, res) => {
-    const _obj = {
-        user_key: req.body.user_key,
-        type: req.body.type,
-        content: req.body.content
-    };
+  const _obj = {
+      user_key: req.body.user_key,
+      type: req.body.type,
+      content: req.body.content
+  };
+  //console.log(_obj.content);
+  //console.log(typeof(_obj.content));
+
+  //안에서 먹을래!
+  if(_obj.content ==  '안에서 먹을래'){
     console.log(_obj.content);
-    console.log(typeof(_obj.content));
-    if(_obj.content ==  '뭐먹을래')
-      console.log("hello");
-    if(JSON.stringify(_obj.content) == '뭐먹을래')
-      console.log("hello2");
     let message = {
       "keyboard": {
           "type": "buttons",
           "buttons": [
-              "1",
-              "2",
-              "3"
+              "학식",
+              "기숙사식당-아침",
+              "기숙사식당-점심"
+              "기숙사식당-저녁"
+              "교직원식당-점심"
+              "교직원식당-저녁"
           ]
       },
       "message": {
-          "text": '안녕하세여'
-      },
+        "text": '안녕하세여'
+      }
     };
-    res.send(message);
+  }
+  //밖에서 먹을래!
+  if(_obj.content ==  '밖에서 먹을래'){
+    console.log(_obj.content);
+    let message = {
+      "keyboard": {
+          "type": "buttons",
+          "buttons": [
+              "종류로 검색",
+              "음식으로 검색",
+          ]
+      },
+      "message": {
+        "text": '안녕하세여'
+      }
+    };
+  }
+  //밖 > 종류
+  if(_obj.content ==  '종류로 검색'){
+    console.log(_obj.content);
+    let message = {
+      "keyboard": {
+          "type": "buttons",
+          "buttons": [
+              "한식",
+              "일식",
+              "중식",
+              "양식",
+              "기타"
+          ]
+      },
+      "message": {
+        "text": '안녕하세여'
+      }
+    };
+  }
+  //밖 > 음식
+  if(_obj.content ==  '음식으로 검색'){
+    console.log(_obj.content);
+    let message = {
+      "keyboard": {
+          "type": "text"    
+      },
+      "message": {
+        "text": '안녕하세여'
+      }
+    };
+  }  
+
+  res.send(message);
 
 });
 module.exports = router;
