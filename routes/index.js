ec8c14d1606f229eba9d3b20a396496f5d91e459
HONG-PC\Hong <blcocas@naver.com>
2017-12-01 23:14:38 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -131,7 +131,10 @@ router.post('/message', (req, res) => {
       for(var i = 0; i<rows.length;i++){
         console.log("1");
         console.log(rows[i].Rest_Name);
-          tmp += rows[i].Rest_Name + '\n';
+          if(rows.length-1 == i)
+            tmp += rows[i].Rest_Name;
+          else
+            tmp += rows[i].Rest_Name + '\n';
       }
          
       let cb = function(){
